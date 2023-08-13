31f436e82ff6a8ce64e682dc6b0346e11c7052ac
kjmq1234 <kjmq1234@naver.com>
2023-08-14 01:31:55 +0900

    2941 add

---
+++ backjun/2941.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int	is_alpha(char* str)
+{
+	if (strncmp(str, "c=", 2) == 0 || strncmp(str, "c-", 2) == 0 || strncmp(str, "d-", 2) == 0 ||
+		strncmp(str, "lj", 2) == 0 || strncmp(str, "nj", 2) == 0 || strncmp(str, "s=", 2) == 0 ||
+		strncmp(str, "z=", 2) == 0)
+		return (2);
+	else if (strncmp(str, "dz=", 3) == 0)
+		return (3);
+	else
+		return (1);
+}
+
+int	main(void)
+{
+	char* arr;
+	int		n;
+	int		i;
+
+	n = 0;
+	arr = (char*)malloc(sizeof(char) * (101));
+	scanf("%s", arr);
+	while (*arr != '\0')
+	{
+		i = is_alpha(arr);
+		arr += i;
+		n++;
+	}
+	printf("%d", n);
+}

3e81cd9db7ba2facc4a71a58ca73fc6f52809280
jammin <imyourdata@soongsil.ac.kr>
2024-02-02 18:37:57 +0900

    add_1417

--- BruteForce/1476.cpp
+++ BruteForce/1476.cpp
@@ -6,8 +6,7 @@ int main(void)
 {
     int E = 0, S = 0, M = 0, year = 1;
 
-    cin >> E >> S >> M;
-    
+    cin >> E >> S >> M; 
     while (true)
     {
         if (((year - E) % 15 == 0) && ((year - S) % 28 == 0) && ((year - M) % 19 == 0))

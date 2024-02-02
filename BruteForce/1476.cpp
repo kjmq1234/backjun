57a83861a5e00476862d3422ad69fca7979b80cf
jammin <imyourdata@soongsil.ac.kr>
2024-02-02 18:36:53 +0900

    init

---
+++ BruteForce/1476.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    int E = 0, S = 0, M = 0, year = 1;
+
+    cin >> E >> S >> M;
+    
+    while (true)
+    {
+        if (((year - E) % 15 == 0) && ((year - S) % 28 == 0) && ((year - M) % 19 == 0))
+        {
+            cout << year;
+            break;
+        }
+        else
+            year++;
+    }
+}
